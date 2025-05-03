c7d9cbf2a73c031fc677b3062d185220c6270978
mfontosco <mstrings11@gmail.com>
2025-05-03 08:54:29 +0100

    chore: added frontend installation and tailwind css setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex justify-center py-4">
+      <h1 className="text-2xl ">Welcome to my url shortener application</h1>
+    </div>
+  );
+}
+
+export default App;
